ad754484d86c8cbc05291e5486348b91f8588d93
Alan Thomas Shaji <50659499+Captain-shaji-stark@users.noreply.github.com>
2023-02-04 18:53:32 +0530

    cleaning time

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import React, {useState, useEffect} from "react";
-import Card from "./components/Card";
-import Navbar from "./components/Navbar";
-import axios from "axios";
+import React from "react";
+import Home from "./components/Home";
 
 const App = () => {
-  const [newsData, setNewsData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    axios.get("http://localhost:3001/api/data")
-      .then((response) => setNewsData(response.data))
-      .catch((error) => console.error(error));
-    setLoading(false);
-  }, []);
-
   return (
-    <div className="App">
-      <Navbar />
-      {newsData?.nodes?.map((newsFeed) => (
-        <Card
-          key={newsFeed?.node.nid}
-          title={newsFeed?.node.title}
-          imageLink={newsFeed?.node.field_photo_image_section}
-        />
-      ))}
-    </div>
+    <Home />
   );
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Card from "./components/Card";
-import Navbar from "./components/Navbar";
+import Card from "./Card";
+import Navbar from "./Navbar";
 import axios from "axios";
 
 const Home = () => {
